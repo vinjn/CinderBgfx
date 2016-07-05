36477fdd1e0bb6097b425b4e155e907bee482dcd
vinjn <vinjn.z@gmail.com>
2016-07-05 22:40:12 +0800

    Make it build.

--- bgfx/include/bx/thread.h
+++ bgfx/include/bx/thread.h
@@ -8,6 +8,9 @@
 
 #if BX_PLATFORM_POSIX
 #	include <pthread.h>
+#	if defined(__FreeBSD__)
+#		include <pthread_np.h>
+#	endif
 #	if defined(__GLIBC__) && !( (__GLIBC__ > 2) || ( (__GLIBC__ == 2) && (__GLIBC_MINOR__ >= 12) ) )
 #		include <sys/prctl.h>
 #	endif // defined(__GLIBC__) ...
@@ -34,7 +37,7 @@ namespace bx
 
 	public:
 		Thread()
-#if BX_PLATFORM_WINDOWS || BX_PLATFORM_XBOX360 || BX_PLATFORM_WINRT
+#if BX_PLATFORM_WINDOWS || BX_PLATFORM_XBOX360 || BX_PLATFORM_XBOXONE || BX_PLATFORM_WINRT
 			: m_handle(INVALID_HANDLE_VALUE)
 			, m_threadId(UINT32_MAX)
 #elif BX_PLATFORM_POSIX
@@ -65,7 +68,7 @@ namespace bx
 			m_stackSize = _stackSize;
 			m_running = true;
 
-#if BX_PLATFORM_WINDOWS || BX_PLATFORM_XBOX360
+#if BX_PLATFORM_WINDOWS || BX_PLATFORM_XBOX360 || BX_PLATFORM_XBOXONE
 			m_handle = ::CreateThread(NULL
 				, m_stackSize
 				, (LPTHREAD_START_ROUTINE)threadFunc
@@ -103,6 +106,8 @@ namespace bx
 
 			result = pthread_create(&m_handle, &attr, &threadFunc, this);
 			BX_CHECK(0 == result, "pthread_attr_setschedparam failed! %d", result);
+#else
+#	error "Not implemented!"
 #endif // BX_PLATFORM_
 
 			m_sem.wait();
@@ -159,7 +164,11 @@ namespace bx
 			prctl(PR_SET_NAME,_name, 0, 0, 0);
 #	endif // defined(__GLIBC__) ...
 #elif BX_PLATFORM_BSD
-			pthread_setname_np(m_handle, _name);
+#ifdef __NetBSD__
+			pthread_setname_np(m_handle, "%s", (void *)_name);
+#else
+			pthread_set_name_np(m_handle, _name);
+#endif
 #elif BX_PLATFORM_WINDOWS && BX_COMPILER_MSVC
 #	pragma pack(push, 8)
 			struct ThreadName
@@ -224,7 +233,7 @@ namespace bx
 		}
 #endif // BX_PLATFORM_
 
-#if BX_PLATFORM_WINDOWS || BX_PLATFORM_XBOX360 || BX_PLATFORM_WINRT
+#if BX_PLATFORM_WINDOWS || BX_PLATFORM_XBOX360 || BX_PLATFORM_XBOXONE || BX_PLATFORM_WINRT
 		HANDLE m_handle;
 		DWORD  m_threadId;
 #elif BX_PLATFORM_POSIX
@@ -269,7 +278,7 @@ namespace bx
 		uint32_t m_id;
 	};
 
-#elif !BX_PLATFORM_WINRT
+#elif !(BX_PLATFORM_XBOXONE || BX_PLATFORM_WINRT)
 
 	class TlsData
 	{
